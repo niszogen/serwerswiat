49a7241b7c3856a657a62785e1306bbb921aefd1
niszogen <43617163+niszogen@users.noreply.github.com>
2020-10-12 22:46:36 +0200

    2 commit

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1602516340",
+        "cacheTag": "1602535060",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1602514462,
+            "last_rendertime": 1602531895,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
